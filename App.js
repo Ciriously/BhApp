b11d08abae2e4f0ddd3c2aca72e45ef8c56edaec
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-07-12 23:28:48 +0530

    Fixed sheet changes

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Animated, ScrollView } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Modal, Image, FlatList } from "react-native";
 import * as Font from 'expo-font';
-import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { Picker } from '@react-native-picker/picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import hatIcon from "./assets/hatIcon.png";
+import homeIcon from "./assets/homeIcon.png";
+import heartIcon from "./assets/heartIcon.png";
+import bellIcon from "./assets/bellIcon.png";
+import userIcon from "./assets/userIcon.png";
+import cogIcon from "./assets/cogIcon.png";
 
-const BottomSheet = () => {
-  const [isSheetOpen, setIsSheetOpen] = useState(false);
-  const bottomSheetHeight = 300; // Increase the height to display more items
-  const animatedSheetHeight = useState(new Animated.Value(0))[0];
-  const [selectedMonth, setSelectedMonth] = useState('');
-
-  const toggleSheet = () => {
-    const toValue = isSheetOpen ? 0 : bottomSheetHeight;
-    Animated.timing(animatedSheetHeight, {
-      toValue: toValue,
-      duration: 300,
-      useNativeDriver: false,
-    }).start();
-    setIsSheetOpen(!isSheetOpen);
-  };
-
+const BillingSummaryPage = ({ transactions, navigation }) => {
   const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [selectedMonth, setSelectedMonth] = useState('');
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     const loadFonts = async () => {
@@ -29,143 +24,334 @@ const BottomSheet = () => {
       });
       setFontsLoaded(true);
     };
+
     loadFonts();
   }, []);
 
-  const renderMonths = () => {
-    const months = [
-      { name: 'January', hasDot: false },
-      { name: 'February', hasDot: false },
-      { name: 'March', hasDot: false },
-      { name: 'April', hasDot: false },
-      { name: 'May', hasDot: false },
-      { name: 'June', hasDot: false },
-      { name: 'July', hasDot: false },
-      { name: 'August', hasDot: false },
-      { name: 'September', hasDot: true },
-      { name: 'October', hasDot: false },
-      { name: 'November', hasDot: false },
-      { name: 'December', hasDot: false },
-    ];
-
-    return months.map((month, index) => (
-      <TouchableOpacity
-        key={index}
-        style={[styles.monthItem, index !== months.length - 1 && styles.monthItemSpacing]}
-        onPress={() => setSelectedMonth(month.name)}
-      >
-        <View style={styles.monthTextContainer}>
-          <Text style={[styles.monthText, month.name === 'September' && styles.blueText]}>
-            {month.name}
-          </Text>
-          {month.hasDot && (
-            <MaterialIcons name="fiber-manual-record" size={8} color="#6658D3" style={styles.dotIcon} />
-          )}
-        </View>
-        <Text style={styles.yearText}>{new Date().getFullYear()}</Text>
-      </TouchableOpacity>
-    ));
+  const handleOpenModal = () => {
+    setIsModalVisible(true);
   };
 
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const listData = [
+    { label: 'Plan Value', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "₹799", rightTextStyle: { color: 'black', fontFamily: 'Gordita-Bold', fontSize: 14 } },
+    { label: 'Discount Applied', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "₹100", rightTextStyle: { color: 'black', fontFamily: 'Gordita-Bold' } },
+    { label: 'Amount paid', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "₹699", rightTextStyle: { color: '#6658D3', fontFamily: 'Gordita-Bold' } },
+    { label: 'Date of Payment', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "24/4/2022", rightTextStyle: { color: 'black', fontFamily: 'Gordita-Bold' } },
+    { label: 'Payee Name', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "Aditya.D.Mishra", rightTextStyle: { color: '#6658D3', fontFamily: 'Gordita-Bold' } },
+    { label: 'Payment ID', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "BH2021VERSION1", rightTextStyle: { color: 'black', fontFamily: 'Gordita-Bold' } },
+    { label: 'Smart Plan', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "Lifetime-Active", rightTextStyle: { color: '#6658D3', fontFamily: 'Gordita-Bold' } },
+    { label: 'Duration', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "Till Graduate", rightTextStyle: { color: 'black', fontFamily: 'Gordita-Bold' } },
+    { label: 'Recurring Pay', leftTextStyle: { color: 'black', fontFamily: 'Gordita-Regular' }, rightText: "No Charges", rightTextStyle: { color: 'black', fontFamily: 'Gordita-Bold' } },
+  ];
+
+  const totalPay = { label: 'Total Amount Paid', 
+  leftTextStyle: { color: 'black', fontFamily: 'Gordita-Bold', fontSize: 20 ,borderRadius: 30, padding: 12, margin: 1,   },
+   rightText: "₹699.00", rightTextStyle: { color: 'black', fontFamily: 'Gordita-Bold', fontSize: 20 } };
+
   if (!fontsLoaded) {
-    // Return a loading state or any other component while the fonts are being loaded
-    return null;
+    return null; // or render a loading indicator
   }
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.monthSelector} onPress={toggleSheet}>
-        <Text style={styles.monthSelectorText}>
-          {selectedMonth || 'Select Month'}
-        </Text>
-        <MaterialIcons name={isSheetOpen ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={24} color="black" />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+          <View style={styles.backButtonCircleOutline}>
+            <View style={styles.backButtonCircle}>
+              <Ionicons name="arrow-back" size={24} color="gray" style={styles.backIcon} />
+            </View>
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.headerText}>Billing</Text>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.smallCircleButton}>
+          <Image source={require('./assets/smallCircle.png')} style={styles.smallCircleIcon} resizeMode="contain" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.billingDetailsContainer}>
+        <Text style={styles.billingDetailsHeader}>Billing Details For</Text>
+        <Text style={styles.billingDetailsSubtext}>Account: Adityad12@gmail.com</Text>
+      </View>
+      <TouchableOpacity style={styles.monthContainer} onPress={handleOpenModal}>
+        <Text style={styles.monthLabel}>{selectedMonth ? selectedMonth : 'Select Month'}</Text>
+        <Icon name="caret-down" size={16} color="black" style={styles.dropdownIcon} />
       </TouchableOpacity>
-      <Animated.View style={[styles.bottomSheet, { height: animatedSheetHeight }]}>
-        <ScrollView contentContainerStyle={styles.monthsContainer}>
-          {renderMonths()}
-        </ScrollView>
-      </Animated.View>
-    </View>
+
+      <Modal visible={isModalVisible} transparent={true} onRequestClose={handleCloseModal}>
+        <TouchableOpacity style={styles.modalBackground} onPress={handleCloseModal} />
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Picker
+              style={styles.monthPicker}
+              selectedValue={selectedMonth}
+              onValueChange={(itemValue) => setSelectedMonth(itemValue)}
+            >
+              <Picker.Item label="Select Month" value="" />
+              <Picker.Item label="January" value="January" />
+              <Picker.Item label="February" value="February" />
+              <Picker.Item label="March" value="March" />
+              {/* Add more months as needed */}
+            </Picker>
+            <TouchableOpacity style={styles.modalCloseButton} onPress={handleCloseModal}>
+              <Text style={styles.modalCloseButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <View style={styles.offersContainer}>
+        <View style={styles.offerSection}>
+          <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+            <View style={styles.hatIcon}>
+              <Image source={hatIcon} style={styles.hatIconImage} resizeMode="contain" />
+            </View>
+          </TouchableOpacity>
+          <Text style={styles.offersHeaderText}>Rs 799 Lifetime</Text>
+        </View>
+        <View style={[styles.offerSection, styles.offerContainer]}>
+          <Text style={styles.offerText}>One Time payment for allmaterials till you Graduate</Text>
+        </View>
+      </View>
+
+      <View style={styles.transactionListContainer}>
+        <FlatList
+          data={listData}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.listItem}>
+              <Text style={[styles.listItemLeftText, item.leftTextStyle]}>{item.label}</Text>
+              <Text style={[styles.listItemRightText, item.rightTextStyle]}>{item.rightText}</Text>
+            </View>
+          )}
+        />
+      </View>
+      
+      <View style={styles.totalPayContainer}>
+        <Text style={[styles.listItemLeftText, totalPay.leftTextStyle]}>{totalPay.label}</Text>
+        <Text style={[styles.listItemRightText, totalPay.rightTextStyle]}>{totalPay.rightText}</Text>
+      </View>
+
+      <View style={styles.footerContainer}>
+        <View style={styles.footerRow}>
+          <TouchableOpacity style={styles.footerIconContainer}>
+            <Image source={homeIcon} style={styles.footerIcon} resizeMode="contain" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.footerIconContainer}>
+            <Image source={heartIcon} style={styles.footerIcon} resizeMode="contain" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.footerIconContainer}>
+            <Image source={bellIcon} style={styles.footerIcon} resizeMode="contain" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.footerIconContainer}>
+            <Image source={userIcon} style={styles.footerIcon} resizeMode="contain" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.footerIconContainer}>
+            <Image source={cogIcon} style={styles.footerIcon} resizeMode="contain" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
+    fontFamily: "Gordita-Bold",
+  },
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 16,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  billingDetailsContainer: {
+    marginBottom: 16,
+  },
+  billingDetailsHeader: {
+    fontSize: 12,
+    fontFamily: "Gordita-Bold",
+    color: "#888888",
+  },
+  billingDetailsSubtext: {
+    fontSize: 18,
+    marginTop: 4,
+    fontFamily: "Gordita-Bold",
+  },
+  monthContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 18,
+    backgroundColor: '#F0F0F0',
+    paddingVertical: 8,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    height: 50,
+  },
+  monthLabel: {
+    flex: 1,
+    fontSize: 16,
+    fontFamily: 'Gordita-Bold',
+    color: '#888888',
+  },
+  modalBackground: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 10,
+    width: '80%',
+  },
+  monthPicker: {
+    fontFamily: 'Gordita-Bold',
+  },
+  modalCloseButton: {
+    marginTop: 16,
+    alignSelf: 'flex-end',
+  },
+  modalCloseButtonText: {
+    fontSize: 16,
+    fontFamily: 'Gordita-Bold',
+    color: 'blue',
+  },
+  offersContainer: {
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 16,
+    marginBottom: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  offerSection: {
+    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  monthSelector: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    backgroundColor: '#F4F7F8',
-    borderWidth: 1,
-    borderColor: '#F4F7F8',
-    borderRadius: 8,
-    marginBottom: 20,
-    width: '80%',
-  },
-  monthSelectorText: {
+  offersHeaderText: {
     fontSize: 16,
     fontFamily: 'Gordita-Bold',
-    marginRight: 5,
-    color: '#6B6B6B',
-  },
-  button: {
-    padding: 10,
-    backgroundColor: 'blue',
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 18,
-  },
-  bottomSheet: {
-    backgroundColor: 'white',
-    width: '100%',
-    position: 'absolute',
-    bottom: 0,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    paddingHorizontal: 20,
-  },
-  monthsContainer: {
-    flexGrow: 1,
-  },
-  monthItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-  },
-  monthItemSpacing: {
-    marginBottom: 10,
-  },
-  monthTextContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  monthText: {
-    fontSize: 16,
-    fontFamily: 'Gordita-Bold',
-    marginLeft: 5,
-    color: '#8E8E8E',
-  },
-  blueText: {
     color: '#6658D3',
+    textAlign: 'center',
+    flex: 1,
   },
-  dotIcon: {
-    marginLeft: 5,
+  offerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
-  yearText: {
-    fontSize: 16,
+  smallCircleButton: {
+    alignSelf: "flex-end",
+    padding: 8,
+    marginTop: 15,
+  },
+  smallCircleIcon: {
+    width: 30,
+    height: 30,
+    alignSelf: "flex-end",
+  },
+  offerText: {
+    fontSize: 12,
+    color: 'gray',
+    marginRight: 8,
+    fontFamily: "Gordita-Bold",
+  },
+  transactionListContainer: {
+    flex: 1,
+    borderRadius: 10,
+    padding: 16,
+    backgroundColor: '#F2F2F2',
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 12,
+  },
+  listItemLeftText: {
+    fontSize: 14,
+    fontFamily: 'Gordita-Bold',
+  },
+  listItemRightText: {
+    fontSize: 12,
     fontFamily: 'Gordita-Regular',
-    color: '#CDCDCD',
+  },
+  totalPayContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 12,
+    backgroundColor: 'white',
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderTopWidth: 1,
+    borderTopColor: 'lightblue',
+  },
+  footerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  footerIconContainer: {
+    alignItems: 'center',
+  },
+  footerIcon: {
+    width: 24,
+    height: 24,
+  },
+  backButton: {
+    borderRadius: 50,
+    padding: 5,
+  },
+  // backButtonCircleOutline: {
+  //   borderWidth: 1,
+  //   borderColor: 'gray',
+  //   borderRadius: 50,
+  // },
+  backButtonCircle: {
+    backgroundColor: '#F2F2F2',
+    borderRadius: 50,
+    padding: 5,
+  },
+  backIcon: {
+    alignSelf: 'center',
+    color: 'gray',
+  },
+  // hatIcon: {
+  //   backgroundColor: '#6658D3',
+  //   borderRadius: 50,
+  //   padding: 8,
+  // },
+
+  hatIconImage: {
+    width: 40,
+    height: 40,
+    alignSelf: 'center',
   },
 });
 
-export default BottomSheet;
+export default BillingSummaryPage;
